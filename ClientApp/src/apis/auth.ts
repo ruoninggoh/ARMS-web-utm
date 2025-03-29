c98e61d04ee673242c791e5ae1378668f1747c42
chloe <gohning@graduate.utm.my>
2025-03-29 21:22:38 +0800

    feat: update auth api

--- ClientApp/src/apis/auth.ts
+++ ClientApp/src/apis/auth.ts
@@ -1,36 +1,3 @@
-// const API_BASE_URL = 'https://localhost:44377/api/Account';
-
-// interface LoginResponse {
-//   token: string;
-//   refreshToken?: string;
-//   expiresIn?: number;
-//   user: {
-//     id: string;
-//     name: string;
-//     utmId: string;
-//   };
-// }
-
-// export const login = async (
-//   utmId: string,
-//   password: string,
-// ): Promise<LoginResponse> => {
-//   try {
-//     const response = await axios.post<LoginResponse>(
-//       `${API_BASE_URL}/login`,
-//       { utmId, password },
-//       {
-//         headers: {
-//           'Content-Type': 'application/json',
-//         },
-//       },
-//     );
-
-//     return response.data;
-//   } catch (error: any) {
-//     throw new Error(error.response?.data?.message || 'Login failed');
-//   }
-// };
 import axios from 'axios';
 
 const API_BASE_URL =
