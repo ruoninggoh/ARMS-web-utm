853fa16b54bc68922f7b4f490d24ef6bd0b56c7e
chloe <gohning@graduate.utm.my>
2025-03-29 21:15:49 +0800

    feat : update url of bff

--- ClientApp/src/apis/auth.ts
+++ ClientApp/src/apis/auth.ts
@@ -34,7 +34,7 @@
 import axios from 'axios';
 
 const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:7224/api';
+  process.env.NEXT_PUBLIC_API_BASE_URL || 'https://localhost:7224/api';
 
 export const loginUser = async (utmId: string, password: string) => {
   try {
