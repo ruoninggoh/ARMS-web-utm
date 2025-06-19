870fe9948dfbe7a4c6096b14c84c0ffc0caa8316
chloe <gohning@graduate.utm.my>
2025-06-20 03:06:21 +0800

    feat: update

--- ClientApp/gatsby-config.ts
+++ ClientApp/gatsby-config.ts
@@ -1,10 +1,13 @@
 import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
-  // Since `gatsby-plugin-typescript` is automatically included in Gatsby you
-  // don't need to define it here (just if you need to change the options)
-  plugins: [`gatsby-plugin-pnpm`],
-  jsxRuntime: `automatic`,
+  plugins: [
+    'gatsby-plugin-pnpm',
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+  ],
+  jsxRuntime: 'automatic',
 };
 
 export default config;
