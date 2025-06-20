150e2e37dbe7810f0f7eb39998bd084ba5ffd939
chloe <gohning@graduate.utm.my>
2025-06-20 17:05:42 +0800

    fix: approval  SSR errors

---
+++ ClientApp/src/components/pages/Approval.tsx
@@ -0,0 +1,32 @@
+// src/pages/approval.tsx
+import ApprovalManagement from '@/components/Approval/ApprovalManagement';
+import Footer from '@/components/Layout/Footer/footer';
+import RoleBasedHeader from '@/components/Layout/Header/RoleBasedHeader';
+import React from 'react';
+import styled from 'styled-components';
+
+const Approval: React.FC = () => {
+  return (
+    <Container>
+      <RoleBasedHeader />
+      <MainContent>
+        <ApprovalManagement />
+      </MainContent>
+      <Footer />
+    </Container>
+  );
+};
+
+export default Approval;
+
+const Container = styled.div`
+  background-color: #f8f9fa;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
+
+const MainContent = styled.main`
+  flex: 1;
+  margin-bottom: 5rem;
+`;
--- ClientApp/src/pages/approval/index.tsx
+++ ClientApp/src/pages/approval/index.tsx
@@ -1,32 +1,8 @@
-// src/pages/approval.tsx
-import ApprovalManagement from '@/components/Approval/ApprovalManagement';
-import Footer from '@/components/Layout/Footer/footer';
-import RoleBasedHeader from '@/components/Layout/Header/RoleBasedHeader';
-import React from 'react';
-import styled from 'styled-components';
+// src/pages/ApprovalPage.tsx
+import loadable from '@loadable/component';
 
-const Approval: React.FC = () => {
-  return (
-    <Container>
-      <RoleBasedHeader />
-      <MainContent>
-        <ApprovalManagement />
-      </MainContent>
-      <Footer />
-    </Container>
-  );
-};
+const ApprovalPage = loadable(() => import('@/components/pages/Approval'), {
+  fallback: <p>Loading...</p>,
+});
 
-export default Approval;
-
-const Container = styled.div`
-  background-color: #f8f9fa;
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-`;
-
-const MainContent = styled.main`
-  flex: 1;
-  margin-bottom: 5rem;
-`;
+export default ApprovalPage;
